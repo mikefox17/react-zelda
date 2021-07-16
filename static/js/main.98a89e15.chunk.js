ab2e21385a4c5eb00e3f29930e5a7367c1e492d9
Michael Fox <mikefox17@gmail.com>
2021-07-16 13:22:14 -0400

    Updates

---
+++ static/js/main.98a89e15.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-zelda"]=this["webpackJsonpreact-zelda"]||[]).push([[0],{10:function(e,t,c){"use strict";c.r(t);var n=c(1),r=c.n(n),s=c(3),a=c.n(s),i=(c(8),c(0));var d=function(){return Object(i.jsx)("div",{className:"App",children:Object(i.jsx)("h2",{children:"Hello there"})})};a.a.render(Object(i.jsx)(r.a.StrictMode,{children:Object(i.jsx)(d,{})}),document.getElementById("root"))},8:function(e,t,c){}},[[10,1,2]]]);
+//# sourceMappingURL=main.98a89e15.chunk.js.map
