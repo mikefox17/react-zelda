5953b04ec4720ec8c21de03caa71357c8504e642
Michael Fox <mikefox17@gmail.com>
2021-07-19 19:03:04 -0400

    added logic for controls

--- src/App.js
+++ src/App.js
@@ -1,11 +1,60 @@
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
 
-function App() {
+export default function App() {
+    const canvasRef = useRef();
+    const [x, setX] = useState(0);
+    const [y, setY] = useState(0);
+
+    useEffect(() => {
+        const context = canvasRef.current.getContext('2d');
+        context.canvas.height = window.innerHeight;
+        context.canvas.width = window.innerWidth;
+    }, []);
+
+    useEffect(() => {
+        const context = canvasRef.current.getContext('2d');
+        context.clearRect(0, 0, window.innerWidth, window.innerHeight);
+        context.fillRect(x, y, 100, 100);
+    }, [x, y]);
+
+    useEffect(() => {
+        window.addEventListener('keydown', handleKeyDown);
+
+        function handleKeyDown(e) {
+            if (e.key === 'ArrowUp') move('up');
+            if (e.key === 'ArrowLeft') move('left');
+            if (e.key === 'ArrowDown') move('down');
+            if (e.key === 'ArrowRight') move('right');
+        }
+
+        return () => window.removeEventListener('keydown', handleKeyDown);
+    }, []);
+
+    function move(direction) {
+        if (direction === 'up') setY(y => y - 20);
+        if (direction === 'left') setX(x => x - 20);
+        if (direction === 'down') setY(y => y + 20);
+        if (direction === 'right') setX(x => x + 20);
+    }
+
     return (
-        <div className='App'>
-            <h2>Hello there</h2>
+        <div className='app'>
+            <canvas ref={canvasRef} />
+
+            <div className='arrows'>
+                <button onClick={() => move('up')}>Up</button>
+                <button onClick={() => move('left')}>Left</button>
+                <button onClick={() => move('down')}>Down</button>
+                <button onClick={() => move('right')}>Right</button>
+            </div>
+
+            <div className='images'>
+                <img src='https://i.imgur.com/JYUB0m3.png' alt='Down' />
+                <img src='https://i.imgur.com/GEXD7bk.gif' alt='Right' />
+                <img src='https://i.imgur.com/XSA2Oom.gif' alt='Up' />
+                <img src='https://i.imgur.com/4LGAZ8t.gif' alt='Left' />
+            </div>
         </div>
     );
 }
-
-export default App;
